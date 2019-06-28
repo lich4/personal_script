951847a81beac4f2aa26dc4f382ac6f0f1592137
lichao.890427@163.com <lichao.890427@chromium.org>
2019-06-28 18:50:06 +0800

    add send recv

---
+++ Frida_script/recvfrom.js
@@ -0,0 +1,31 @@
+{
+  gargs: null,
+  onEnter: function (log, args, state) {
+	this.gargs = new Array(6);
+	for (var i = 0; i < 6; i++) this.gargs[i] = args[i];
+  },
+  onLeave: function (log, retval, state) {
+	var args = this.gargs;
+    var sockfd = args[0].toInt32();
+	var buf = args[1];
+	var len = args[2].toInt32();
+	console.log('dst_addr='+args[4]);
+	var dest_addr = args[4];
+	if (dest_addr.equals(0)) {
+		dest_addr = Memory.alloc(16);
+		var addr_len = Memory.alloc(4);
+		Memory.writeU32(addr_len, 16);
+		var getpeername = new NativeFunction(Module.findExportByName(null, "getpeername"), "int", ["int", "pointer", "pointer"]);
+		getpeername(sockfd, dest_addr, addr_len);
+	}
+	var sin_family = Memory.readU8(dest_addr.add(1));
+	var sin_port = Memory.readU16(dest_addr.add(2));
+	sin_port = ((sin_port&0xff)<<8)|((sin_port>>8)&0xff);
+	var sin_addr = Memory.readU32(dest_addr.add(4));
+	var sin_ip = (sin_addr&0xff).toString() + '.' + ((sin_addr>>8)&0xff).toString() +
+		'.' + ((sin_addr>>16)&0xff).toString() + '.' + ((sin_addr>>24)&0xff).toString();
+    log("recvfrom(sockfd=" + args[0] + ",buflen=" + len + ",family=" + sin_family + 
+		",ip=" + sin_ip + ",port=" + sin_port + ")");
+	log(hexdump(buf,{length:len,header:false}));
+  }
+}
