4d0c6499b390139fa14938da66c90c6c14c5488a
lich0 <lichao.890427@163.com>
2020-02-08 10:44:10 +0800

    Update ios_dump.js

--- Frida_script/ios_dump.js
+++ Frida_script/ios_dump.js
@@ -163,6 +163,9 @@ var read = getExportFunction("f", "read", "int", ["int", "pointer", "int"]);
 var write = getExportFunction("f", "write", "int", ["int", "pointer", "int"]);
 var lseek = getExportFunction("f", "lseek", "int64", ["int", "int64", "int"]);
 var close = getExportFunction("f", "close", "int", ["int"]);
+var remove = getExportFunction("f", "remove", "int", ["pointer"]);
+var access = getExportFunction("f", "access", "int", ["pointer", "int"]);
+var dlopen = getExportFunction("f", "dlopen", "pointer", ["pointer", "int"]);
 
 function getCacheDir(index) {
 	var NSUserDomainMask = 1;
@@ -205,104 +208,177 @@ var LC_SEGMENT_64 = 0x19;
 var LC_ENCRYPTION_INFO = 0x21;
 var LC_ENCRYPTION_INFO_64 = 0x2C;
 
-// You can dump .app or dylib (Encrypt/No Encrypt)
+function getDocumentDir() {
+    var NSDocumentDirectory = 9;
+    var NSUserDomainMask = 1;
+    var npdirs = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, 1);
+    return ObjC.Object(npdirs).objectAtIndex_(0).toString();
+}
+
+var FAT_MAGIC = 0xcafebabe;
+var FAT_CIGAM = 0xbebafeca;
+var MH_MAGIC = 0xfeedface;
+var MH_CIGAM = 0xcefaedfe;
+var MH_MAGIC_64 = 0xfeedfacf;
+var MH_CIGAM_64 = 0xcffaedfe;
+var LC_SEGMENT = 0x1;
+var LC_SEGMENT_64 = 0x19;
+var LC_ENCRYPTION_INFO = 0x21;
+var LC_ENCRYPTION_INFO_64 = 0x2C;
+
+function pad(str, n) {
+    return Array(n-str.length+1).join("0")+str;
+}
+
+function swap32(value) {
+    value = pad(value.toString(16),8)
+    var result = "";
+    for(var i = 0; i < value.length; i=i+2){
+        result += value.charAt(value.length - i - 2);
+        result += value.charAt(value.length - i - 1);
+    }
+    return parseInt(result,16)
+}
+
+function dumpMemory(base, size, offset) {
+    base = ptr(base);
+    var newmodpath = getDocumentDir() + "/mem.dmp";
+    if(!access(allocStr(newmodpath),0)){
+        remove(allocStr(newmodpath));
+    }
+    var fmodule = open(newmodpath, O_CREAT | O_WRONLY, 0);
+    if (offset) lseek(fmodule, offset, SEEK_SET);
+    write(fmodule, base, size);
+    close(fmodule);
+    console.log('dump to ' + newmodpath);
+}
+
 function dumpModule(name) {
-	if (modules == null) {
-		modules = getAllAppModules();
-	}
-	var targetmod = null;
-	for (var i = 0; i < modules.length; i++) {
-		if (modules[i].path.indexOf(name) != -1) {
-			targetmod = modules[i];
-			break;
-		}
-	}
-	if (targetmod == null) {
-		console.log("Cannot find module");
-	}
-	var modbase = modules[i].base;
-	var modsize = modules[i].size;
-	var newmodname = modules[i].name + ".decrypted";
-	var finddir = false;
-	var newmodpath = "";
-	var fmodule = -1;
-	var index = 1;
-	while (!finddir) { // 找到一个可写路径
-		try {
-			var base = getCacheDir(index);
-			if (base != null) {
-				newmodpath = getCacheDir(index) + "/" + newmodname;
-				fmodule = open(newmodpath, O_CREAT | O_RDWR, 0);
-				if (fmodule != -1) {
-					break;
-				};
-			}
-		}
-		catch(e) {
-		}
-		index++;
-	}
-	
-	var oldmodpath = modules[i].path;
-	var foldmodule = open(oldmodpath, O_RDONLY, 0);
-	if (fmodule == -1 || foldmodule == -1) {
-		console.log("Cannot open file" + newmodpath);
-		return;
-	}
+    if (modules == null) {
+        modules = getAllAppModules();
+    }
 
-	var BUFSIZE = 4096;
-	var buffer = malloc(BUFSIZE);
-	while (read(foldmodule, buffer, BUFSIZE)) {
-		write(fmodule, buffer, BUFSIZE);
-	}
-	
-	// Find crypt info and recover
-	var is64bit = false;
-	var size_of_mach_header = 0;
-	var magic = getU32(modbase);
-	if (magic == MH_MAGIC || magic == MH_CIGAM) {
-		is64bit = false;
-		size_of_mach_header = 28;
-	}
-	else if (magic == MH_MAGIC_64 || magic == MH_CIGAM_64) {
-		is64bit = true;
-		size_of_mach_header = 32;
-	}
-	var ncmds = getU32(modbase.add(16));
-	var off = size_of_mach_header;
-	var offset_cryptoff = -1;
-	var crypt_off = 0;
-	var crypt_size = 0;
-	var segments = [];
-	for (var i = 0; i < ncmds; i++) {
-		var cmd = getU32(modbase.add(off));
-		var cmdsize = getU32(modbase.add(off + 4)); 
-		if (cmd == LC_ENCRYPTION_INFO || cmd == LC_ENCRYPTION_INFO_64) {
-			offset_cryptoff = off + 8;
-			crypt_off = getU32(modbase.add(off + 8));
-			crypt_size = getU32(modbase.add(off + 12));
-		}
-		off += cmdsize;
-	}
+    var targetmod = null;
+    for (var i = 0; i < modules.length; i++) {
+        if (modules[i].path.indexOf(name) != -1) {
+            targetmod = modules[i];
+            break;
+        }
+    }
+    if (targetmod == null) {
+        console.log("Cannot find module");
+        return;
+    }
+    var modbase = modules[i].base;
+    var modsize = modules[i].size;
+    var newmodname = modules[i].name;
+    var newmodpath = getDocumentDir() + "/" + newmodname + ".fid";
+    var oldmodpath = modules[i].path;
 
-	if (offset_cryptoff != -1) {
-		var tpbuf = malloc(8);
-		console.log("Fix decrypted at:" + offset_cryptoff.toString(16));
-		putU64(tpbuf, 0);
-		lseek(fmodule, offset_cryptoff, SEEK_SET);
-		write(fmodule, tpbuf, 8);
-		console.log("Fix decrypted at:" + crypt_off.toString(16));
-		lseek(fmodule, crypt_off, SEEK_SET);
-		write(fmodule, modbase.add(crypt_off), crypt_size);
-	}
-	console.log("Decrypted file at:" + newmodpath + " 0x" + modsize.toString(16));
-	close(fmodule);
-	close(foldmodule);
-}	
 
-function dumpheader() {
-    var base = Process.enumerateModulesSync()[0].base;
-    var magic = Memory.readU32(base);
+    if(!access(allocStr(newmodpath),0)){
+        remove(allocStr(newmodpath));
+    }
+
+    var fmodule = open(newmodpath, O_CREAT | O_RDWR, 0);
+    var foldmodule = open(oldmodpath, O_RDONLY, 0);
+
+    if (fmodule == -1 || foldmodule == -1) {
+        console.log("Cannot open file" + newmodpath);
+        return;
+    }
+
+    var is64bit = false;
+    var size_of_mach_header = 0;
+    var magic = getU32(modbase);
+    var cur_cpu_type = getU32(modbase.add(4));
+    var cur_cpu_subtype = getU32(modbase.add(8));
+    if (magic == MH_MAGIC || magic == MH_CIGAM) {
+        is64bit = false;
+        size_of_mach_header = 28;
+    }else if (magic == MH_MAGIC_64 || magic == MH_CIGAM_64) {
+        is64bit = true;
+        size_of_mach_header = 32;
+    }
+
+    var BUFSIZE = 4096;
+    var buffer = malloc(BUFSIZE);
+
+    read(foldmodule, buffer, BUFSIZE);
+
+    var fileoffset = 0;
+    var filesize = 0;
+    magic = getU32(buffer);
+    if(magic == FAT_CIGAM || magic == FAT_MAGIC){
+        var off = 4;
+        var archs = swap32(getU32(buffer.add(off)));
+        for (var i = 0; i < archs; i++) {
+            var cputype = swap32(getU32(buffer.add(off + 4)));
+            var cpusubtype = swap32(getU32(buffer.add(off + 8)));
+            if(cur_cpu_type == cputype && cur_cpu_subtype == cpusubtype){
+                fileoffset = swap32(getU32(buffer.add(off + 12)));
+                filesize = swap32(getU32(buffer.add(off + 16)));
+                break;
+            }
+            off += 20;
+        }
+
+        if(fileoffset == 0 || filesize == 0)
+            return;
+
+        lseek(fmodule, 0, SEEK_SET);
+        lseek(foldmodule, fileoffset, SEEK_SET);
+        for(var i = 0; i < parseInt(filesize / BUFSIZE); i++) {
+            read(foldmodule, buffer, BUFSIZE);
+            write(fmodule, buffer, BUFSIZE);
+        }
+        if(filesize % BUFSIZE){
+            read(foldmodule, buffer, filesize % BUFSIZE);
+            write(fmodule, buffer, filesize % BUFSIZE);
+        }
+    }else{
+        var readLen = 0;
+        lseek(foldmodule, 0, SEEK_SET);
+        lseek(fmodule, 0, SEEK_SET);
+        while(readLen = read(foldmodule, buffer, BUFSIZE)) {
+            write(fmodule, buffer, readLen);
+        }
+    }
+
+    var ncmds = getU32(modbase.add(16));
+    var off = size_of_mach_header;
+    var offset_cryptid = -1;
+    var crypt_off = 0;
+    var crypt_size = 0;
+    var segments = [];
+    for (var i = 0; i < ncmds; i++) {
+        var cmd = getU32(modbase.add(off));
+        var cmdsize = getU32(modbase.add(off + 4));
+        if (cmd == LC_ENCRYPTION_INFO || cmd == LC_ENCRYPTION_INFO_64) {
+            offset_cryptid = off + 16;
+            crypt_off = getU32(modbase.add(off + 8));
+            crypt_size = getU32(modbase.add(off + 12));
+        }
+        off += cmdsize;
+    }
+
+    if (offset_cryptid != -1) {
+        var tpbuf = malloc(8);
+        putU64(tpbuf, 0);
+        lseek(fmodule, offset_cryptid, SEEK_SET);
+        write(fmodule, tpbuf, 4);
+        lseek(fmodule, crypt_off, SEEK_SET);
+        write(fmodule, modbase.add(crypt_off), crypt_size);
+        console.log('modbase', modbase,'cryptoff',crypt_off,'crypt_size',crypt_size,'cryptid',offset_cryptid);
+    }
+
+    close(fmodule);
+    close(foldmodule);
+    return newmodpath
+}
+
+function dumpHeader(base) {
+    var magic = base.readU32();
     var is64bit = false;
     if (magic == 0xfeedfacf) {
         is64bit = true;
@@ -311,21 +387,33 @@ function dumpheader() {
     } else {
         console.log('Unknown magic:' + magic);
     }
-    var cmdnum = Memory.readU32(base.add(0x10));
+    var cmdnum = base.add(0x10).readU32();
     var cmdoff = is64bit?0x20:0x1C;
     for (var i = 0; i < cmdnum; i++) {
-        var cmd = Memory.readU32(base.add(cmdoff));
-        var cmdsize = Memory.readU32(base.add(cmdoff + 4));
+        var cmd = base.add(cmdoff).readU32();
+        var cmdsize = base.add(cmdoff + 4).readU32();
         cmdoff += cmdsize;
         if (cmd == 1) { // SEGMENT
-            var segname = Memory.readUtf8String(base.add(cmdoff + 8))
-            var vmaddr = Memory.readU32(base.add(cmdoff + 0x18));
-            var vmsize = Memory.readU32(base.add(cmdoff + 0x1C));
+            var segname = base.add(cmdoff + 8).readUtf8String();
+            var vmaddr = base.add(cmdoff + 0x18).readU32();
+            var vmsize = base.add(cmdoff + 0x1C).readU32();
+            var nsects = base.add(cmdoff + 0x40).readU8();
+            var secbase = base.add(cmdoff + 0x38);
+            if (base.add(cmdoff + 4).readU32() >= 0x38 + nsects*68)
+            for (var i = 0; i < nsects; i++) {
+                console.log('\t' + i + '/' + nsects + '-' + secbase.add(i*68).readUtf8String());
+            }
             console.log(segname + ' ' + vmaddr.toString(16) + '-' + (vmaddr+vmsize).toString(16));
         } else if (cmd == 25) { // SEGMENT_64
-            var segname = Memory.readUtf8String(base.add(cmdoff + 8))
-            var vmaddr = Memory.readU32(base.add(cmdoff + 0x18));
-            var vmsize = Memory.readU32(base.add(cmdoff + 0x20));
+            var segname = base.add(cmdoff + 8).readUtf8String();
+            var vmaddr = base.add(cmdoff + 0x18).readU32();
+            var vmsize = base.add(cmdoff + 0x20).readU32();
+            var nsects = base.add(cmdoff + 0x40).readU8();
+            var secbase = base.add(cmdoff + 0x48);
+            if (base.add(cmdoff + 4).readU32() >= 0x48 + nsects*80)
+            for (var i = 0; i < nsects; i++) {
+                console.log('\t' + i + '/' + nsects + '-' + secbase.add(i*80).readUtf8String());
+            }
             console.log(segname + ' ' + vmaddr.toString(16) + '-' + (vmaddr+vmsize).toString(16));
         }
     }
