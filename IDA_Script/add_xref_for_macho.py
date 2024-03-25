1ff64bb204b0bb53a13e7da3f8479ac90f701854
lich0 <lichao.890427@163.com>
2024-03-25 11:52:17 +0800

    Update add_xref_for_macho.py
    support IDA7.7

--- IDA_Script/add_xref_for_macho.py
+++ IDA_Script/add_xref_for_macho.py
@@ -1,9 +1,10 @@
 import idc
 import idautils
 
-is64 = get_inf_structure().is_64bit()
+is64 = True
 ptsize = 8 if is64 else 4
-GetPointer = Qword if is64 else Dword
+get_pt = get_qword if is64 else get_wide_dword
+b2s = lambda d: d.decode() if d is not None and type(d) == bytes and str != bytes else d
 
 def addxref(sel, f_ea):
     add_cref(sel, f_ea, XREF_USER | fl_F)
@@ -80,25 +81,26 @@ def addobjcref():
         print("cannot find __objc_selrefs")
         return
     for selref_ea in range(seg.start_ea, seg.end_ea, ptsize):
-        sel_ea = GetPointer(selref_ea)
-        sel_name = GetString(sel_ea)
+        sel_ea = get_pt(selref_ea)
+        sel_name = b2s(get_strlit_contents(sel_ea))
         if sel_name not in forbit_meth:
             sel_map[sel_name] = sel_ea
     # get objc func table
     for addr in idautils.Functions():
-        func_name = get_name(addr)
-        if len(func_name) >= 6 and func_name[1] == "[": # +[? ?] -[? ?]
+        func_name = b2s(get_name(addr))
+        if func_name[0] in ['+', '-'] and func_name[1] == '[' and func_name[-1] == ']': # +[? ?] -[? ?]
             sel_name = func_name[2:-1].split(" ")[-1]
             # may be more than one function with same sel but differenct class
-            if sel_name not in imp_map:
-                imp_map[sel_name] = []
-            imp_map[sel_name].append(addr)
+            if sel_name not in forbit_meth:
+                if sel_name not in imp_map:
+                    imp_map[sel_name] = []
+                imp_map[sel_name].append(addr)
     # make xref
     for (sel_name, sel_ea) in sel_map.items():
         if sel_name in imp_map:
             for f_addr in imp_map[sel_name]:
                 addxref(sel_ea, f_addr)
-            print "added xref for " + sel_name
+            print("added xref for " + sel_name)
 
 if __name__ == "__main__":
     addobjcref()
