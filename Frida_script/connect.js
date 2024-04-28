0dca8683cf9402c3c9d3d3ed079599546bf07f6b
lich0 <lichao.890427@163.com>
2024-04-28 22:00:38 +0800

    Create connect.js

---
+++ Frida_script/connect.js
@@ -0,0 +1,17 @@
+{
+	onEnter: function (log, args, state) {
+		var sockfd = args[0].toInt32();
+		var dest_addr = args[1];
+		var sin_family = Memory.readU8(dest_addr.add(1));
+		var sin_port = Memory.readU16(dest_addr.add(2));
+		sin_port = ((sin_port & 0xff) << 8) | ((sin_port >> 8) & 0xff);
+		var sin_addr = Memory.readU32(dest_addr.add(4));
+		var sin_ip = (sin_addr & 0xff).toString() + '.' + ((sin_addr >> 8) & 0xff).toString() +
+			'.' + ((sin_addr >> 16) & 0xff).toString() + '.' + ((sin_addr >> 24) & 0xff).toString();
+		log("connect(sockfd=" + sockfd + ",family=" + sin_family +
+			",ip=" + sin_ip + ",port=" + sin_port + ")");
+	},
+	onLeave: function (log, retval, state) {
+		log('->' + retval)
+	}
+}
