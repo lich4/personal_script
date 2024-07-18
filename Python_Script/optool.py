0bea3e166352a1a53b23bee5324208d91aa4e121
lich0 <lichao.890427@163.com>
2024-07-18 17:56:37 +0800

    Update optool.py

--- Python_Script/optool.py
+++ Python_Script/optool.py
@@ -1,12 +1,11 @@
-#!python3.9
+#!python
 
 import argparse
-import lief
 import os
 import sys
 import tempfile
-from lief.MachO import LOAD_COMMAND_TYPES as LCT
-from lief.MachO import CPU_TYPES as CT
+import lief
+from lief.MachO import *
 
 ver = "v1.0"
 
@@ -19,22 +18,22 @@ EMPTY_ENT = """<?xml version="1.0" encoding="UTF-8"?>
 """
 
 dylib_keymap = {
-    LCT.ID_DYLIB:           "id", 
-    LCT.LOAD_DYLIB:         "load", 
-    LCT.REEXPORT_DYLIB:     "reexport", 
-    LCT.LOAD_WEAK_DYLIB:    "weak", 
-    LCT.LAZY_LOAD_DYLIB:    "lazy", 
-    LCT.LOAD_UPWARD_DYLIB:  "upwardoad", 
-    LCT.PREBOUND_DYLIB:     "prebound",
+    LoadCommand.TYPE.ID_DYLIB:           "id", 
+    LoadCommand.TYPE.LOAD_DYLIB:         "load", 
+    LoadCommand.TYPE.REEXPORT_DYLIB:     "reexport", 
+    LoadCommand.TYPE.LOAD_WEAK_DYLIB:    "weak", 
+    LoadCommand.TYPE.LAZY_LOAD_DYLIB:    "lazy", 
+    LoadCommand.TYPE.LOAD_UPWARD_DYLIB:  "upwardoad", 
+    LoadCommand.TYPE.PREBOUND_DYLIB:     "prebound",
 }
 
 dylib_ctor_map = {
-    "id":       lief.MachO.DylibCommand.id_dylib,
-    "load":     lief.MachO.DylibCommand.load_dylib,
-    "reexport": lief.MachO.DylibCommand.reexport_dylib,
-    "weak":     lief.MachO.DylibCommand.weak_lib,
-    "lazy":     lief.MachO.DylibCommand.lazy_load_dylib,
-    "upwardoad":lief.MachO.DylibCommand.load_upward_dylib,
+    "id":       DylibCommand.id_dylib,
+    "load":     DylibCommand.load_dylib,
+    "reexport": DylibCommand.reexport_dylib,
+    "weak":     DylibCommand.weak_lib,
+    "lazy":     DylibCommand.lazy_load_dylib,
+    "upwardoad":DylibCommand.load_upward_dylib,
 }
 
 version2str = lambda version: ".".join([str(i) for i in version])
@@ -42,16 +41,15 @@ version2int = lambda version: (version[2]) | (version[1] << 8) | (version[0] << 
 
 def get_arch(cpu_type, cpu_subtype):
     cpu_keymap = {
-        CT.ARM: "arm",
-        CT.ARM64: "arm64",
-        CT.I386: "i386",
-        CT.x86: "x86",
-        CT.x86_64: "x86_64",
+        Header.CPU_TYPE.ARM: "arm",
+        Header.CPU_TYPE.ARM64: "arm64",
+        Header.CPU_TYPE.X86: "x86",
+        Header.CPU_TYPE.X86_64: "x86_64",
     }
     if cpu_type not in cpu_keymap:
         return "unknown"
     s = cpu_keymap[cpu_type]
-    if cpu_type == CT.ARM64 and cpu_subtype > 0:
+    if cpu_type == Header.CPU_TYPE.ARM64 and cpu_subtype > 0:
         s += "e abi=" + hex(cpu_subtype)
     return s
 
@@ -60,8 +58,8 @@ class MachOFile:
         self.path = path
         self.valid = lief.is_macho(path)
         if self.valid:
-            self.obj = lief.MachO.parse(path, config=lief.MachO.ParserConfig.quick)
-            if lief.MachO.is_fat(path):
+            self.obj = parse(path, config=ParserConfig.quick)
+            if is_fat(path):
                 self.fatnum = len(self.obj)
             else:
                 self.fatnum = 0
@@ -74,7 +72,7 @@ class MachOFile:
             os.system("lipo -create -output {} {}".format(path, path))
         return True
 
-def inst_dylib(args, obj=None):
+def add_dylib(args, obj=None):
     need_update = False
     if obj:
         name = args.loadpath
@@ -98,14 +96,14 @@ def inst_dylib(args, obj=None):
     for obj in f.obj:
         header = obj.header
         print("{} (architecture {})".format(name, get_arch(header.cpu_type, header.cpu_subtype)))
-        if inst_dylib(args, obj):
+        if add_dylib(args, obj):
             need_update = True
     if need_update:
         path = args.output if args.output else args.target
         print("Successfully write to " + path)
         f.write(path)
 
-def replace_dylib(args, obj=None):
+def chg_dylib(args, obj=None):
     need_update = False
     if obj:
         index_max = len(obj.commands)
@@ -120,13 +118,13 @@ def replace_dylib(args, obj=None):
                     continue
                 to_del_index = index
                 ctor = dylib_ctor_map[dylib_type]
-                new_cmd = ctor(args.replace, cmd.timestamp, version2int(cmd.current_version), version2int(cmd.compatibility_version))
+                new_cmd = ctor(args.chg_dylib, cmd.timestamp, version2int(cmd.current_version), version2int(cmd.compatibility_version))
                 need_update = True
                 break
         if to_del_index:
             obj.remove_command(to_del_index)
             obj.add(new_cmd, to_del_index)
-            print("Successfully update [{}/{}] {} -> {}".format(index, index_max, args.loadpath, args.replace))
+            print("Successfully update [{}/{}] {} -> {}".format(index, index_max, args.loadpath, args.chg_dylib))
         return need_update
     target = args.target
     if not os.path.exists(target):
@@ -140,14 +138,14 @@ def replace_dylib(args, obj=None):
     for obj in f.obj:
         header = obj.header
         print("{} (architecture {})".format(name, get_arch(header.cpu_type, header.cpu_subtype)))
-        if replace_dylib(args, obj):
+        if chg_dylib(args, obj):
             need_update = True
     if need_update:
         path = args.output if args.output else args.target
         print("Successfully write to " + path)
         f.write(path)
 
-def uninst_dylib(args, obj=None):
+def del_dylib(args, obj=None):
     need_update = False
     if obj:
         index_max = len(obj.commands)
@@ -180,15 +178,49 @@ def uninst_dylib(args, obj=None):
     for obj in f.obj:
         header = obj.header
         print("{} (architecture {})".format(name, get_arch(header.cpu_type, header.cpu_subtype)))
-        if uninst_dylib(args, obj):
+        if del_dylib(args, obj):
             need_update = True
     if need_update:
         path = args.output if args.output else args.target
         print("Successfully write to " + path)
         f.write(path)
 
+def add_rpath(args, obj=None):
+    need_update = False
+    if obj:
+        path = args.runpath
+        command = RPathCommand.rpath(path)
+        if args.index != -1:
+            obj.add(command, args.index)
+        else:
+            obj.add(command)
+        print("Successfully insert {}".format(path))
+        return True
+    target = args.target
+    if not os.path.exists(target):
+        print("target file not exist")
+        return
+    name = os.path.basename(target)
+    f = MachOFile(target)
+    if not f.valid:
+        print("macho invalid")
+        return
+    for obj in f.obj:
+        header = obj.header
+        print("{} (architecture {})".format(name, get_arch(header.cpu_type, header.cpu_subtype)))
+        if add_rpath(args, obj):
+            need_update = True
+    if need_update:
+        path = args.output if args.output else args.target
+        print("Successfully write to " + path)
+        f.write(path)
+
+def del_rpath(args, obj=None):
+    pass
+
 def list_dylib(args, obj=None):
     if obj:
+        rpath_list = list()
         dylib_dict = {k:list() for k in dylib_keymap.values()}
         index_max = len(obj.commands)
         for index, cmd in enumerate(obj.commands):
@@ -200,8 +232,17 @@ def list_dylib(args, obj=None):
                     "ver": cmd.current_version,
                     "compatver": cmd.compatibility_version,
                 })
+            elif cmd.command == LoadCommand.TYPE.RPATH:
+                rpath_list.append({
+                    "index": index,
+                    "path": cmd.path
+                })
+        if rpath_list:
+            print("  command rpath:")
+            for item in rpath_list:
+                print("    [{}/{}] {}".format(item["index"], index_max, item["path"]))
         for k in dylib_keymap.values():
-            if len(dylib_dict[k]) == 0:
+            if not dylib_dict[k]:
                 continue
             print("  command dylib {}:".format(k))
             for item in dylib_dict[k]:
@@ -227,46 +268,61 @@ def list_dylib(args, obj=None):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("optool " + ver, add_help=False)
     subparsers = parser.add_subparsers()
-    inst_parser = subparsers.add_parser("install", add_help=False, help="Inserts an LC_LOAD command into the target binary with specific loadpath")
-    replace_parser = subparsers.add_parser("replace", add_help=False, help="Replace any LC_LOAD commands which point to a given loadpath from the target binary")
-    uninst_parser = subparsers.add_parser("uninstall", add_help=False, help="Removes any LC_LOAD commands which point to a given loadpath from the target binary")
-    list_parser = subparsers.add_parser("list", add_help=False, help="Show app dependency dylibs in target binary")
-    inst_parser.add_argument("-c", type=str, dest="command", required=True)
-    inst_parser.add_argument("-p", type=str, dest="loadpath", required=True)
-    inst_parser.add_argument("-i", type=int, dest="index", default=-1)
-    inst_parser.add_argument("-t", type=str, dest="target", required=True)
-    inst_parser.add_argument("-o", type=str, dest="output")
-    inst_parser.set_defaults(func=inst_dylib)
-    replace_parser.add_argument("-c", type=str, dest="command")
-    replace_parser.add_argument("-p", type=str, dest="loadpath", required=True)
-    replace_parser.add_argument("-r", type=str, dest="replace", required=True)
-    replace_parser.add_argument("-t", type=str, dest="target", required=True)
-    replace_parser.add_argument("-o", type=str, dest="output")
-    replace_parser.set_defaults(func=replace_dylib)
-    uninst_parser.add_argument("-c", type=str, dest="command")
-    uninst_parser.add_argument("-p", type=str, dest="loadpath", required=True)
-    uninst_parser.add_argument("-t", type=str, dest="target", required=True)
-    uninst_parser.add_argument("-o", type=str, dest="output")
-    uninst_parser.set_defaults(func=uninst_dylib)
-    list_parser.add_argument("-t", type=str, dest="target", required=True)
-    list_parser.set_defaults(func=list_dylib)
+    adddylib_parser = subparsers.add_parser("add_dylib", add_help=False)
+    chgdylib_parser = subparsers.add_parser("chg_dylib", add_help=False)
+    deldylib_parser = subparsers.add_parser("del_dylib", add_help=False)
+    addrpath_parser = subparsers.add_parser("add_rpath", add_help=False)
+    delrpath_parser = subparsers.add_parser("del_rpath", add_help=False)
+    info_parser = subparsers.add_parser("info", add_help=False)
+    adddylib_parser.add_argument("-c", type=str, dest="command", required=True)
+    adddylib_parser.add_argument("-p", type=str, dest="loadpath", required=True)
+    adddylib_parser.add_argument("-i", type=int, dest="index", default=-1)
+    adddylib_parser.add_argument("-t", type=str, dest="target", required=True)
+    adddylib_parser.add_argument("-o", type=str, dest="output")
+    adddylib_parser.set_defaults(func=add_dylib)
+    chgdylib_parser.add_argument("-c", type=str, dest="command")
+    chgdylib_parser.add_argument("-p", type=str, dest="loadpath", required=True)
+    chgdylib_parser.add_argument("-r", type=str, dest="relace", required=True)
+    chgdylib_parser.add_argument("-t", type=str, dest="target", required=True)
+    chgdylib_parser.add_argument("-o", type=str, dest="output")
+    chgdylib_parser.set_defaults(func=chg_dylib)
+    deldylib_parser.add_argument("-c", type=str, dest="command")
+    deldylib_parser.add_argument("-p", type=str, dest="loadpath", required=True)
+    deldylib_parser.add_argument("-t", type=str, dest="target", required=True)
+    deldylib_parser.add_argument("-o", type=str, dest="output")
+    deldylib_parser.set_defaults(func=del_dylib)
+    addrpath_parser.add_argument("-p", type=str, dest="runpath", required=True)
+    addrpath_parser.add_argument("-i", type=int, dest="index", default=-1)
+    addrpath_parser.add_argument("-t", type=str, dest="target", required=True)
+    addrpath_parser.add_argument("-o", type=str, dest="output")
+    addrpath_parser.set_defaults(func=add_rpath)
+    delrpath_parser.add_argument("-p", type=str, dest="runpath", required=True)
+    delrpath_parser.add_argument("-t", type=str, dest="target", required=True)
+    delrpath_parser.add_argument("-o", type=str, dest="output")
+    delrpath_parser.set_defaults(func=del_rpath)
+    info_parser.add_argument("-t", type=str, dest="target", required=True)
+    info_parser.set_defaults(func=list_dylib)
     args = parser.parse_args()
 
     if hasattr(args, "func"):
         args.func(args)
     else:
         HELP = parser.format_usage()
-        HELP += "install -c <command> -p <loadpath> [-i <index>] -t <target> [-o <output>]      \n"
-        HELP += "    Insert LC_LOAD command into target binary with specific loadpath           \n"
-        HELP += "replace -c <command> -p <loadpath> [-r <replace>] -t <target> [-o <output>]    \n"
-        HELP += "    Replace LC_LOAD command from target binary with specific loadpath          \n"
-        HELP += "uninstall [-c <command>] -p <loadpath> -t <target> [-o <output>]               \n"
-        HELP += "    Remove LC_LOAD command from target binary with specific loadpath           \n"
-        HELP += "list -t <target>                                                         \n"
+        HELP += "add_dylib -c <command> -p <loadpath> [-i <index>] -t <target> [-o <output>]    \n"
+        HELP += "    Insert LC_LOAD command with specific loadpath into target binary           \n"
+        HELP += "chg_dylib -c <command> -p <loadpath> [-r <replace>] -t <target> [-o <output>]  \n"
+        HELP += "    Replace LC_LOAD command with specific loadpath from target binary          \n"
+        HELP += "del_dylib [-c <command>] -p <loadpath> -t <target> [-o <output>]               \n"
+        HELP += "    Remove LC_LOAD command with specific loadpath from target binary           \n"
+        HELP += "add_rpath -p <runpath> [-i <index>] -t <target> [-o <output>]                  \n"
+        HELP += "    Insert LC_RPATH command with specific runpath into target binary           \n"
+        HELP += "del_rpath -p <runpath> -t <target> [-o <output>]                               \n"
+        HELP += "    Remove LC_RPATH command with specific runpath from target binary           \n"
+        HELP += "info -t <target>                                                               \n"
         HELP += "    show app dependent dylibs in target binary                                 \n"
         HELP += "\nOPTIONS:                                                                     \n"
         HELP += "    -t <target>    Input macho file                                            \n"
-        HELP += "    -p <loadpath>  Dylib path to install or uninstall                          \n"
+        HELP += "    -p <loadpath>  Dylib path to add_dylib or del_dylib                        \n"
         HELP += "    -c <command>   Type of load command                                        \n"
         HELP += "        id:        LC_ID_DYLIB                                                 \n"
         HELP += "        lazy:      LC_LAZY_LOAD_DYLIB                                          \n"
@@ -275,15 +331,18 @@ if __name__ == "__main__":
         HELP += "        upward:    LC_LOAD_UPWARD_DYLIB                                        \n"
         HELP += "        weak:      LC_LOAD_WEAK_DYLIB                                          \n"
         HELP += "    -o <path>      Output macho file, default to Input file                    \n"
-        HELP += "    -i <index>     Insert position to install                                  \n"
-        HELP += "    -r <path>      Old path to replace for install command                     \n"
+        HELP += "    -i <index>     Insert position to add_dylib                                \n"
+        HELP += "    -r <replace>   New path to replace for chg_dylib command                   \n"
         print(HELP)
+
+
 
 '''
 test:
-python3 optool.py list -t /tmp/ls
-python3 optool.py uninstall -p /usr/lib/libutil.dylib -t /tmp/ls
-python3 optool.py replace -p /usr/lib/libutil.dylib -r /usr/lib/test1.dylib -t /tmp/ls
-python3 optool.py install -c weak -p /usr/lib/libutil.dylib -t /tmp/ls
+python3 optool.py info -t /tmp/ls
+python3 optool.py del_dylib -p /usr/lib/libutil.dylib -t /tmp/ls
+python3 optool.py chg_dylib -p /usr/lib/libutil.dylib -r /usr/lib/test1.dylib -t /tmp/ls
+python3 optool.py add_dylib -c weak -p /usr/lib/libutil.dylib -t /tmp/ls
 '''
+
 
